b065887ad8fb8e028c4f623b9a84f311745e8c7e
vani0123 <40710300+vani0123@users.noreply.github.com>
2019-04-23 05:58:47 -0500

    Update MySeleniumTest.java
    changes for jenkins integration

--- project/src/test/java/artifactory/test/MySeleniumTest.java
+++ project/src/test/java/artifactory/test/MySeleniumTest.java
@@ -9,13 +9,15 @@ import org.testng.annotations.Test;
 public class MySeleniumTest {
 	//public static String drivePath = "C:\\Users\\AK Jain\\Desktop\\vani\\golden gate DevOps\\chromedriver_win32\\";
 	//public static String drivePath ="resources/";
+	//docker,jenkins,tomcat install on this develop-machine gcp only
 	@Test
 	public static void TestPageContent() {
 		System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-
-		WebDriver driver = new ChromeDriver();
-
-		driver.get("http://35.200.169.251:8081/project-1.0-RAMA/");
+		ChromeOptions chromeOptions = new ChromeOptions();
+ 		chromeOptions.addArguments("--headless");
+		WebDriver driver = new ChromeDriver(chromeOptions);
+		
+		driver.get("http://35.200.228.144:8081/project-1.0-RAMA/");
 		WebElement TxtContent = driver.findElement(By.xpath("/html/body/p"));
 		if (TxtContent.getText().contentEquals("This session is to demonstrate how to setup simple pipeline")) {
 			System.out.println("Success: Page content is -"+TxtContent.getText());
